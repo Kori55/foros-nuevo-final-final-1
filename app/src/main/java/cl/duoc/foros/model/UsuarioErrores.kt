aad193a700a6fe8b47bf867b386268ed66fff34d
Kori55 <sebast.sepulvedag@duocuc.cl>
2025-11-25 18:56:03 -0300

    "cannot figure out how to read this field from a cursor"???????????

--- app/src/main/java/cl/duoc/foros/model/UsuarioErrores.kt
+++ app/src/main/java/cl/duoc/foros/model/UsuarioErrores.kt
@@ -4,6 +4,5 @@ data class UsuarioErrores(
     val nombre : String ? = null,
     val correo : String? = null,
     val clave : String? = null,
-    val moderador : Boolean? = null,
-    val errores : UsuarioErrores? = null
+    val moderador : Boolean? = null
 )
