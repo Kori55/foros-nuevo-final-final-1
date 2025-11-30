ea0c94b3bfe6f48e7276239debd4e6f8ad786765
Kori55 <sebast.sepulvedag@duocuc.cl>
2025-11-30 12:20:31 -0300

    go go my 50 androidmanifest.xml errors

--- app/src/main/java/cl/duoc/foros/model/Moderador.kt
+++ app/src/main/java/cl/duoc/foros/model/Moderador.kt
@@ -10,7 +10,7 @@ data class Moderador(
         correo = "",
         clave = "",
         terminos = false,
-        errores = UsuarioErrores()
+        errores = UsuarioErrores()k
         )
     // object funcionesModeracion : FuncionesModeracion()
 */
